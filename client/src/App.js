17774adc2344002cd1d191bd17bf01cd2e56ee2e
Lishadsza <22b18.lisha@sjec.ac.in>
2024-10-18 20:57:56 +0530

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,4 @@
-/*
-import './App.css';
-import io from 'socket.io-client'
-const socket=io.connect("http://localhost:3001");
-function App() {
-  return <div className="App">hiiii</div>
 
-}
-
-
-
-export default App;
-
-*/
 import './App.css';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
@@ -19,27 +6,44 @@ import { useEffect, useState } from 'react';
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  
   const [userID, setUserID] = useState('');
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
 
   useEffect(() => {
     
     socket.on('connect', () => {
       console.log(`Connected with ID: ${socket.id}`);
-      setUserID(socket.id);  
+      setUserID(socket.id);
     });
-
-    
     return () => {
       socket.disconnect();
     };
   }, []);
 
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      console.log(`${username} joined room ${room}`);
+    }
+  };
+
   return (
     <div className="App">
       <h3>Message</h3>
-      <input type="text" placeholder="abc..."/>
-      <input type="text" placeholder="abc..."/>
+      <input
+        type="text"
+        placeholder="Your name..."
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(event) => setRoom(event.target.value)}
+      />
+      <button onClick={joinRoom}>Join Chat</button>
+
+      <h4>Connected User ID: {userID}</h4>
     </div>
   );
 }
